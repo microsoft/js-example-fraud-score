caf4d65c5ee303af6cd529424d8903f5da8a7c63
Sean Wells <swells@users.noreply.github.com>
2016-03-21 12:10:17 -0700

    chore (maintenance): update to deployrUtils

--- analytics/ccFraudScore.R
+++ analytics/ccFraudScore.R
@@ -1,9 +1,9 @@
 
 ### SCORING THE MODEL 
-require(RevoScriptTools)
-revoInput('{"name": "bal", "render":"integer", "default": 5000, "min" : 0, "max": 25000 }')
-revoInput('{"name": "trans", "render":"integer", "default": 12, "min" : 0, "max": 100 }')
-revoInput('{"name": "credit", "render":"integer", "default": 8, "min" : 0, "max": 75 }')
+require(deployrUtils)
+deployrInput('{"name": "bal", "render":"integer", "default": 5000, "min" : 0, "max": 25000 }')
+deployrInput('{"name": "trans", "render":"integer", "default": 12, "min" : 0, "max": 100 }')
+deployrInput('{"name": "credit", "render":"integer", "default": 8, "min" : 0, "max": 75 }')
 
 if(!exists('fraudModel')){load('fraudModel.rData')}
 score<-data.frame(balance=bal,numTrans=trans,creditLine=credit)
